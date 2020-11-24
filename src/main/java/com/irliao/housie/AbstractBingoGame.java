a628234519696fde2febdbe612916fca5529a386
Ryan Liao <ianryanliao@gmail.com>
2020-11-23 23:13:06 -0800

    refactored base bingo class and housie make extension simpler

--- src/main/java/com/irliao/housie/AbstractBingoGame.java
+++ src/main/java/com/irliao/housie/AbstractBingoGame.java
@@ -2,6 +2,7 @@ package com.irliao.housie;
 
 import com.irliao.housie.bingo.BingoNumberProvider;
 import com.irliao.housie.bingo.RandomBingoNumberProvider;
+import com.irliao.housie.combination.AbstractCombination;
 import com.irliao.housie.command.CallCommand;
 import com.irliao.housie.command.Command;
 import com.irliao.housie.command.QuitCommand;
@@ -11,9 +12,8 @@ import com.irliao.housie.role.Dealer;
 import com.irliao.housie.role.Player;
 import com.irliao.housie.ticket.Ticket;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /***
@@ -26,41 +26,45 @@ public abstract class AbstractBingoGame {
     public static final String KEY_TO_QUIT = "Q";
     private static final String KEY_TO_CONTINUE = "N";
 
+    private final Map<Integer, Set<AbstractCombination>> currentWinners;
+
     private final Scanner inputScanner;
     private final KeyPressHandler keyPressHandler;
     private Dealer dealer;
     private BingoNumberProvider bingoNumberProvider;
     String gameName;
     List<Player> players;
+    Set<AbstractCombination> winnableCombinations;
 
-    public AbstractBingoGame() {
+    AbstractBingoGame() {
+        currentWinners = new HashMap<>();
         inputScanner = new Scanner(System.in);
         keyPressHandler = new KeyPressHandler();
     }
 
     /***
-     * Define how the winners will be determined.
+     * Sets up the winning combinations that are allowed in this type of bingo.
+     * Requires the subclass to implement this since every type of bingo game
+     * may have different combinations the ticket can win with.
+     * This class will use the provided Set of combinations to determine if
+     * a player's ticket is a winning ticket of that combination.
+     * @return Set of combinations the ticket can win with
      */
-    abstract void determineWinners();
+    abstract Set<AbstractCombination> setUpCombinations();
 
     /***
-     * Defines whether the game is still in progress.
-     * @return true if game still in progress (in loop)
+     * Gets the map of the current winners and the combinations won.
+     * @return map of winning player's ID to the Set of combinations won
      */
-    abstract boolean isGameInProgress();
-
-    /***
-     * Defines the summary of the game.
-     * This summary is to be generated and displayed once the game is over.
-     * @return summary of the game
-     */
-    abstract String generateGameSummary();
+    Map<Integer, Set<AbstractCombination>> getCurrentWinners() {
+        return currentWinners;
+    }
 
     /***
      * Starts the bingo game application, using the implementations specified by the subclass.
      * Will first request user input to set up the game, then execute the game loop.
      */
-    public void play() {
+    void play() {
         displayGameIntro();
 
         registerQuitCommand();
@@ -73,12 +77,13 @@ public abstract class AbstractBingoGame {
         bingoNumberProvider = new RandomBingoNumberProvider(settingHandler.getNumberRangeStart(), settingHandler.getNumberRangeEnd());
         dealer = new Dealer(bingoNumberProvider.createBingoNumbers());
         players = createAndRegisterPlayersWithTicket(settingHandler);
+        winnableCombinations = setUpCombinations();
 
         // register this after the setup has been completed
         registerCallCommand();
 
         // game loop (specific implementations provided by subclass)
-        do {
+        while (isGameInProgress()) {
             try {
                 keyPressHandler.runCommand(inputScanner.nextLine());
                 determineWinners();
@@ -86,7 +91,6 @@ public abstract class AbstractBingoGame {
                 System.out.println(e.getMessage());
             }
         }
-        while (isGameInProgress());
 
         displayGameSummary();
     }
@@ -112,6 +116,47 @@ public abstract class AbstractBingoGame {
         System.out.println("\n***Ticket Created Successfully ****");
 
         return players;
+    }
+
+    /***
+     * Checks the current players' tickets to see if any combination has been won.
+     * Once the combination has been claimed by a player, it is no longer claimable by other players.
+     * The order at which the players are checked is sequential to how the players are created.
+     */
+    void determineWinners() {
+        players.forEach(player ->
+            winnableCombinations.stream()
+                .filter(winCombination -> !winCombination.getClaimed())
+                .forEach(winCombination ->  {
+                    if (winCombination.isWinningTicket(player.getTicket())) {
+                        System.out.println("\n");
+                        System.out.println("We have a winner: Player#" + player.getId() + " has won " + "'" + winCombination.getName() + "'" + " winning combination.");
+                        registerWinner(player.getId(), winCombination);
+                        winCombination.setClaimed(true);
+                    }
+                }));
+    }
+
+    /***
+     * Registers the player's ID and the player's win combinations into a map.
+     * This method should be called when a new winner has been determined.
+     * @param id id of the player who had a winning ticket for the combination
+     * @param combinationWon the combination the player has won
+     */
+    void registerWinner(int id, AbstractCombination combinationWon) {
+        Set<AbstractCombination> combinationsWon = currentWinners.getOrDefault(id, new HashSet<>());
+        combinationsWon.add(combinationWon);
+        currentWinners.put(id, combinationsWon);
+    }
+
+    /***
+     * Determines if the current game is over yet.
+     * The game is over once there are no more winnable combinations left.
+     * @return true if all winnable combinations are claimed
+     */
+    boolean isGameInProgress() {
+        return winnableCombinations.stream()
+            .anyMatch(winCombination -> !winCombination.getClaimed());
     }
 
     /***
@@ -151,4 +196,26 @@ public abstract class AbstractBingoGame {
         System.out.println(generateGameSummary());
         System.out.println("=======================");
     }
+
+    /***
+     * Generates a string of current summary of the game.
+     * The summary contains player ID and the combinations claimed by the player.
+     * The summary should be specific to Housie type bingo.
+     * @return summary of the Housie game
+     */
+    String generateGameSummary() {
+        StringBuilder stringBuilder = new StringBuilder();
+        players.forEach(player -> {
+            Set<AbstractCombination> winCombinations = currentWinners.getOrDefault(player.getId(), new HashSet<>());
+            List<String> winningCombinationsString = winCombinations.stream()
+                .map(AbstractCombination::getName)
+                .collect(Collectors.toList());
+            boolean isWinningTicket = !winningCombinationsString.isEmpty();
+            String playerSummary = isWinningTicket ? String.join(" and ", winningCombinationsString) : "Nothing";
+            stringBuilder.append("Player#").append(player.getId()).append(" : ").append(playerSummary).append("\n");
+        });
+
+        return stringBuilder.toString();
+    }
+
 }
--- src/main/java/com/irliao/housie/Housie.java
+++ src/main/java/com/irliao/housie/Housie.java
@@ -5,97 +5,30 @@ import com.irliao.housie.combination.EarlyFiveCombination;
 import com.irliao.housie.combination.FullHouseCombination;
 import com.irliao.housie.combination.TopLineCombination;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.HashSet;
+import java.util.Set;
 
 /***
  * An implementation of the Housie game.
- * This class implements methods specified from AbstractBingoGame with logic that is specific to Housie.
+ * This class provides the winning combinations the Base bingo class will use to determine winner
+ * and determine when the game is over.
  */
-public class Housie extends AbstractBingoGame {
-
-    private final Set<AbstractCombination> winnableCombinations;
-    private final Map<Integer, Set<AbstractCombination>> currentWinners;
-
-    public Housie() {
+class Housie extends AbstractBingoGame {
+    Housie() {
         gameName = "Housie";
-        currentWinners = new HashMap<>();
+    }
 
-        winnableCombinations = new HashSet<>();
+    /***
+     * Sets up the winning combinations that are allowed in this type of bingo.
+     * Housie specifically allows: Early Five, Full House, and Top Line.
+     * @return Set of combinations the ticket can win with
+     */
+    @Override
+    Set<AbstractCombination> setUpCombinations() {
+        Set<AbstractCombination> winnableCombinations = new HashSet<>();
         winnableCombinations.add(new EarlyFiveCombination());
         winnableCombinations.add(new FullHouseCombination());
         winnableCombinations.add(new TopLineCombination());
-    }
-
-    /***
-     * Gets the map of the current winners and the combinations won.
-     * @return map of winning player's ID to the Set of combinations won
-     */
-    Map<Integer, Set<AbstractCombination>> getCurrentWinners() {
-        return currentWinners;
-    }
-
-    /***
-     * Checks the current players' tickets to see if any combination has been won.
-     * Once the combination has been claimed by a player, it is no longer claimable by other players.
-     * The order at which the players are checked is sequential to how the players are created.
-     */
-    @Override
-    void determineWinners() {
-        players.forEach(player -> winnableCombinations.stream()
-            .filter(winCombination -> !winCombination.getClaimed())
-            .forEach(winCombination ->  {
-                if (winCombination.isWinningTicket(player.getTicket())) {
-                    System.out.println("\n");
-                    System.out.println("We have a winner: Player#" + player.getId() + " has won " + "'" + winCombination.getName() + "'" + " winning combination.");
-                    registerWinner(player.getId(), winCombination);
-                    winCombination.setClaimed(true);
-                }
-            }));
-    }
-
-    /***
-     * Registers the player's ID and the player's win combinations into a map.
-     * This method should be called when a new winner has been determined.
-     * @param id id of the player who had a winning ticket for the combination
-     * @param combinationWon the combination the player has won
-     */
-    public void registerWinner(int id, AbstractCombination combinationWon) {
-        Set<AbstractCombination> combinationsWon = currentWinners.getOrDefault(id, new HashSet<>());
-        combinationsWon.add(combinationWon);
-        currentWinners.put(id, combinationsWon);
-    }
-
-    /***
-     * Determines if the current game is over yet.
-     * The game is over once there are no more winnable combinations left.
-     * @return true if all winnable combinations are claimed
-     */
-    @Override
-    boolean isGameInProgress() {
-        return winnableCombinations.stream()
-                                   .anyMatch(winCombination -> !winCombination.getClaimed());
-    }
-
-    /***
-     * Generates a string of current summary of the game.
-     * The summary contains player ID and the combinations claimed by the player.
-     * The summary should be specific to Housie type bingo.
-     * @return summary of the Housie game
-     */
-    @Override
-    String generateGameSummary() {
-        StringBuilder stringBuilder = new StringBuilder();
-        players.forEach(player -> {
-            Set<AbstractCombination> winCombinations = currentWinners.getOrDefault(player.getId(), new HashSet<>());
-            List<String> winningCombinationsString = winCombinations.stream()
-                .map(AbstractCombination::getName)
-                .collect(Collectors.toList());
-            boolean isWinningTicket = !winningCombinationsString.isEmpty();
-            String playerSummary = isWinningTicket ? String.join(" and ", winningCombinationsString) : "Nothing";
-            stringBuilder.append("Player#").append(player.getId()).append(" : ").append(playerSummary).append("\n");
-        });
-
-        return stringBuilder.toString();
+        return winnableCombinations;
     }
 }
